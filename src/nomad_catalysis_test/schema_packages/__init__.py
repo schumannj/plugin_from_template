a458ea2cfa8a613c776ed8c72c26ebf7ac083565
Julia Schumann <32182254+schumannj@users.noreply.github.com>
2024-05-16 14:24:31 +0000

    added catalyst measurement class from yaml schema

--- src/nomad_catalysis_test/schema_packages/__init__.py
+++ src/nomad_catalysis_test/schema_packages/__init__.py
@@ -7,11 +7,23 @@ class MySchemaPackageEntryPoint(SchemaPackageEntryPoint):
 
     def load(self):
         from nomad_catalysis_test.schema_packages.mypackage import m_package
- 
+
         return m_package
 
+class CatalystMeasurementPackageEntryPoint(SchemaPackageEntryPoint):
+    parameter: int = Field(0, description='Custom configuration parameter')
+
+    def load(self):
+        from nomad_catalysis_test.schema_packages.catalyst_measurement import m_package
+
+        return m_package
 
 mypackage = MySchemaPackageEntryPoint(
     name='MyPackage',
     description='Schema package defined using the new plugin mechanism.',
+)
+
+catalystmeasurement = CatalystMeasurementPackageEntryPoint(
+    name='CatalystMeasurement',
+    description='Catalyst Measurement Schema package defined using the new plugin mechanism.',
 )
